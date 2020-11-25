9454278158706b90ed86733ff97fcda45614b56f
abraao503 <abraao503@gmail.com>
2020-11-25 17:58:14 -0400

    add show message method

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -4,6 +4,7 @@ const messageController = require('../app/controllers/message.controller');
 const messageRouter = Router();
 
 messageRouter.post('/', messageController.store);
+messageRouter.get('/', messageController.index);
 messageRouter.get('/:userId', messageController.show);
 
 module.exports = messageRouter; 
