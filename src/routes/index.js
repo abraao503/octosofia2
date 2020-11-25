de2d9a2f92c3545144ecb7eca944c0919628cfd1
abraao503 <abraao503@gmail.com>
2020-11-25 17:57:16 -0400

    add auth middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
+const auth = require('../app/middlewares/auth.middleware');
 const messageRouter = require('./message.route');
 
 const routes = Router();
 
+routes.use(auth);
 routes.use('/messages/', messageRouter);
 
 module.exports = routes;
